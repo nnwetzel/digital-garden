edcb7b5fb04424a40d8e2235fef7ed981bf8bdc0
Nathaniel Wetzel <nnwetzel@icloud.com>
2025-09-22 00:04:52 -0400

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -25,6 +25,7 @@ export default function Home() {
           <li>Working on a x64 Compiler in <span style={{ color: '#e53935', fontWeight: 500, fontFamily: 'Georgia, serif', fontSize: '1em', lineHeight: '1' }}>C++</span></li>
           <li>Deepening my knowledge in Computer Vision, Computer Systems, and <span style={{ color: '#e53935', fontWeight: 500, fontFamily: 'Georgia, serif', fontSize: '1em', lineHeight: '1' }}>C++</span></li>
           <li>Reading <a href="https://www.goodreads.com/book/show/4406.East_of_Eden">East of Eden</a></li>
+          <li>Exploring letters on <a href="https://we-b.site/">we-b.site</a></li>
         </ul>
         <div style={{ height: "1.5rem" }} />
         <h3 style={{ fontWeight: 700, fontSize: '1.05em', marginBottom: '0.5em' }}>Soon, I will be</h3>
